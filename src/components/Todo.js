09429cea1e7be30fbc10847680f0a47d8add8d38
phamhuuan <phaman3899@gmail.com>
2021-04-08 16:08:56 +0700

    modify todo item

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
@@ -33,10 +26,10 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+        <TodoItem 
+          key={item.key}
+          item={item} 
+        />
       ))}
       <div className="panel-block">
         {items.length} items
