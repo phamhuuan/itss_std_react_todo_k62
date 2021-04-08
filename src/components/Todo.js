8d747f8e520f0c4f619c261285cbf044e9e46d03
phamhuuan <phaman3899@gmail.com>
2021-04-08 16:23:47 +0700

    switch state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
@@ -27,15 +20,6 @@ function Todo() {
     /* テストコード 終了 */
   ]);
   
-  const [filter, setFilter] = React.useState('ALL');
-
-  const displayItems = items.filter(item => {
-    if (filter === 'ALL') return true;
-    if (filter === 'TODO') return !item.done;
-    if (filter === 'DONE') return item.done;
-  });
-
-  
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -45,24 +29,13 @@ function Todo() {
     });
     putItems(newItems);
   };
-  
-  const handleAdd = text => {
-    putItems([...items, { key: getKey(), text, done: false }]);
-  };
-  
-  const handleFilterChange = value => setFilter(value);
 
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      <Input onAdd={handleAdd} />
-      <Filter
-        onChange={handleFilterChange}
-        value={filter}
-      />
-      {displayItems.map(item => (
+      {items.map(item => (
         <TodoItem 
           key={item.key}
           item={item}
@@ -70,7 +43,7 @@ function Todo() {
         />
       ))}
       <div className="panel-block">
-        {displayItems.length} items
+        {items.length} items
       </div>
     </div>
   );
